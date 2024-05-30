6990df8ca36b80d2ce3da009c0a486d30aa73ff0
Matt Manning <mtmanning93@hotmail.co.uk>
2024-05-30 13:27:37 +0200

    Add computers play and start game functions

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 const squares = document.getElementsByClassName("square");
 
-function playO() {
+function startGame() {
+  for (let square of squares) {
+    square.addEventListener("click", function () {
+      userChoice = this.getAttribute("value");
+    });
+  }
+
+  playComp();
+}
+
+function playUser() {
+  // Updates square with 0 when user clicks
   for (let i = 0; i < squares.length; i++) {
     squares[i].addEventListener("click", function () {
       squares[i].innerHTML = "<h1>O</h1>";
@@ -8,4 +19,14 @@ function playO() {
   }
 }
 
-document.addEventListener("DOMContentLoaded", playO);
+function playComp(userChoice) {
+  const randomChoice = Math.floor(Math.random() * squares.length);
+
+  const computerChoice = squares[randomChoice];
+
+  computerChoice.innerHTML = "<h1>X</h1>";
+
+  return computerChoice;
+}
+
+document.addEventListener("DOMContentLoaded", playUser);
